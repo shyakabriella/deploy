2e866027a2f989174df561010e7ce6c4ebc91608
shyakabriella <lindabriella@gmail.com>
2025-02-19 15:43:56 +0200

    newsite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/NavBar';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Navbar/>
+        <p className='Hello'>Hello world!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+function Navbar(){
+
+    return(
+        <div>
+            <a>Home</a>
+            <a>Services</a>
+            <a>About</a>
+        </div>
+
+    )
+};
+export default Navbar;
